0106889ecdbe37fe52cfd48cce00071e8c2c5d69
Seiya-wasabi <se.sato614@gmail.com>
2021-04-17 12:17:12 +0000

    aboutまで完了！！！！！！！！

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :create, :index, :show, :destroy]
   
   resources :users, only: [:show]
+  get "/home/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
