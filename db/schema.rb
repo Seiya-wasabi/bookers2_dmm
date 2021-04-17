197c562bd3d256d80b7a816b471b38e2cc0b4e0d
Seiya-wasabi <se.sato614@gmail.com>
2021-04-17 11:03:59 +0000

    ifでヘッダーの表示内容変更するやつ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_061846) do
+ActiveRecord::Schema.define(version: 2021_04_17_103506) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "book_name"
+    t.string "book_id"
+    t.integer "user_id"
+    t.text "title"
+    t.text "opinion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
