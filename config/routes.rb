bdcc74c1774a9e31bb98642a9623dabc08ee3a13
Seiya-wasabi <se.sato614@gmail.com>
2021-04-21 08:40:43 +0000

    oo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'post_images/new'
+  get 'post_images/create'
+  get 'post_images/index'
+  get 'post_images/show'
+  get 'post_images/destroy'
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :destroy]
-
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show,:edit]
   resources :homes
   get "/home/about" => "homes#about"
