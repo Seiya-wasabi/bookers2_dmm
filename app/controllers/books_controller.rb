461b2a19b30b2707e62f1c2d7a2017c0b25d9f3a
Seiya-wasabi <se.sato614@gmail.com>
2021-04-22 05:41:28 +0000

    一旦画像関係を削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
-    @user = User.find(params[:user_id])
+    # @user = User.find(params[:user_id])
   end
 
   def show
